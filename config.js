838ca1564b410db7adfc20afdd2289b82a5c53a6
Hieu05 <hieuhip.05052004gmail.com>
2024-03-05 12:32:46 +0700

    newsroucecode

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Bé iu';
+const giftUrl = 'https://www.facebook.com/behieu2k4/';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng Bé iu Của Anh';
+const contentCard = 'Chúc beiu 8/3 duii dẻ nhooo. Cảm ơn em vì đã ở bên anh. Hy vọng là chúng ta sẽ mãi luôn như vậy.Anh thương bé, anh cũng rất xin lỗi bé vì đã không thể làm dì được nhiều cho bé trong thời gian qua, anh sẽ cố gắng để chúng ta mãi hạnh phúc em nhé.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Hiếu https://www.facebook.com/HieuFF.05/
+const giftImage = 'behien1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
